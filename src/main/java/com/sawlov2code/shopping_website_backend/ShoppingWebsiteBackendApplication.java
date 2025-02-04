969c6d8de9d851f6e4892fa6a4095514641e3fe8
saw <ehkalanephaung96@gmail.com>
2025-02-04 14:53:15 +0700

    initialize Spring Boot project with Gradle setup and PostgreSQL configuration

---
+++ src/main/java/com/sawlov2code/shopping_website_backend/ShoppingWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sawlov2code.shopping_website_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingWebsiteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingWebsiteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sawlov2code/shopping_website_backend/ShoppingWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sawlov2code.shopping_website_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingWebsiteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
